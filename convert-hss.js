f82a6c8b38ef777dc654bab7e7ee4bf6e6632768
drasil <63302357+drasilishere@users.noreply.github.com>
2020-04-07 17:54:25 +0200

    center
    added the css center tag.

--- convert-hss.js
+++ convert-hss.js
@@ -13,8 +13,11 @@ const dothis = string => {
     } else if(string.startsWith("bg")) {
         return `h1, h2, h3, h4, h5, h6, p, a {\nbackground-color: ${string.slice(3)};\n}`
     } else if(string.startsWith("font")) {
-        return `body {\nfont-family: ${string.slice(5)};\n}`
+        return `body {\nfont-family: ${string.slice(5)};\n}`;
+    } else if(string.startsWith("center")) {
+        return `body\n{\ntext-align: center;\n}`;
     }
+
 };
 
 fs.writeFileSync("styles.css", "/*built with hss*/\n" + s.map(dothis).join('\n'));
